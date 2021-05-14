b2bc453262bcbef969f2ef7fef96eb8571ebb763
Evgeniya-star-35 <78686547+Evgeniya-star-35@users.noreply.github.com>
2021-05-14 20:10:29 +0000

    Deploying to gh-pages from @ Evgeniya-star-35/team-project-demo@dc904917a751b3c796f69aaf094100cee1fc2b57 🚀

--- src.11b2cf37.js
+++ src.11b2cf37.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-demo/src.f210c22c.js.map
+//# sourceMappingURL=/team-project-demo/src.11b2cf37.js.map
